ffaa7f300cd942b763db1342261f28cf55ec9cf6
Inspition <252704246@qq.com>
2026-06-25 23:02:32 +0800

    feat: 添加止盈收益计算功能，支持自定义参数并生成收益数据

--- src/algorithm/util.ts
+++ src/algorithm/util.ts
@@ -19,3 +19,76 @@ export function* genrateItems() {
 export function genrateRandomText() {
   return Math.random().toString(36).slice(2, 7)
 }
+
+/**
+ * 计算止盈收益
+ *
+ * @param   {[type]}  options  [options description]
+ *
+ * @return  {[type]}           [return description]
+ */
+export function* calcProfit(
+  options: { bj?: number; sy?: number; jd?: number[]; mc?: number[] } = {},
+) {
+  const { bj, sy, jd, mc } = {
+    bj: 1,
+    sy: 0,
+    jd: [0.2, 0.3, 0.5],
+    mc: [0.25, 0.25, 0.5],
+    ...options,
+  }
+
+  let benj = bj,
+    shouy = sy,
+    maic = 0
+  for (let i = 0; i < jd.length; i++) {
+    const add = jd[i]
+    const hc = mc[i]
+    benj = bj * (1 + add)
+    maic = benj - benj * (1 - hc)
+    benj -= maic
+    shouy += maic
+
+    yield {
+      tip: `第${i + 1}轮止盈`,
+      原始资金: bj,
+      本金: benj,
+      收益: shouy,
+      增长: add,
+      卖出: maic,
+    }
+  }
+}
+// gen = calcProfit({ bj: 5000 })
+
+export function* calcProfitJS(options = {}) {
+  const { bj, sy, jd, mc } = {
+    bj: 1,
+    sy: 0,
+    jd: [0.2, 0.3, 0.5],
+    mc: [0.25, 0.25, 0.5],
+    ...options,
+  }
+
+  let benj = bj,
+    shouy = sy,
+    maic = 0
+  for (let i = 0; i < jd.length; i++) {
+    const add = jd[i]
+    const hc = mc[i]
+    benj = bj * (1 + add)
+    maic = benj - benj * (1 - hc)
+    benj -= maic
+    shouy += maic
+
+    yield {
+      tip: `第${i + 1}轮止盈`,
+      原始资金: bj,
+      本金: benj,
+      收益: shouy,
+      增长: add,
+      卖出: maic,
+    }
+  }
+}
+// gen = calcProfitJS({ bj: 5000 })
